a11ba640266aae3546e01ea847f105b4bab77c08
Shawn Michael <kwesimichael17@gmail.com>
2022-02-11 07:28:17 -0800

    pizza

--- app/src/main/java/com/example/myrestaurant/PizzaActivity.kt
+++ app/src/main/java/com/example/myrestaurant/PizzaActivity.kt
@@ -7,5 +7,7 @@ class PizzaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pizza)
+
+        //cunt
     }
 }
